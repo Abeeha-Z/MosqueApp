fa28e2617bf475ea3ea314b9d0748fc6f19f9285
joe-238 <171643026+joe-238@users.noreply.github.com>
2025-07-14 12:30:57 -0400

    dsadas

--- mosque-app/src/App.vue
+++ mosque-app/src/App.vue
@@ -4,13 +4,13 @@ import Menu from 'primevue/menu';
 </script>
 
 <template>
-  <nav class="flex w-full items-center justify-center gap-5 fixed bg-red-300 py-3 ">
-    <RouterLink to="/">MMSI Sunday School</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/contacts">Contacts</RouterLink>
-    <RouterLink to="/lessons">Lessons</RouterLink>
-    <RouterLink to="/register">Registeration</RouterLink>
-    <button>Login</button>
+  <nav class="flex w-full items-center justify-center gap-5 fixed  py-7 ">
+    <RouterLink class="font-bold text-[120%]" to="/">MMSI Sunday School</RouterLink>
+    <RouterLink class="font-bold text-[120%]" to="/about">About</RouterLink>
+    <RouterLink class="font-bold text-[120%]" to="/contacts">Contacts</RouterLink>
+    <RouterLink class="font-bold text-[120%]" to="/lessons">Lessons</RouterLink>
+    <RouterLink class="font-bold text-[120%]" to="/register">Registeration</RouterLink>
+    <button class="font-bold text-[120%] py-2 px-5 bg-amber-400 rounded-2xl">Login</button>
   </nav>
   <RouterView />
 </template>
--- mosque-app/src/views/HomeView.vue
+++ mosque-app/src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="w-full h-screen flex justify-center  ">
-        <div class="w-[98%] h-[80%] mt-[1%] rounded-2xl bg-amber-500"></div>
+    <div class="w-full h-screen flex flex-col items-center gap-4  ">
+        <div class="w-[98%] h-[80%] mt-[1%] rounded-2xl bg-amber-500 flex items-center">
+            <h3 class="font-bold text-3xl w-[40%]">rando inspiring text rando inspiring text rando inspiring text rando inspiring text</h3>
+        </div>
+        <h2 class="font-bold text-3xl">Inspire Kids to Love Islam </h2>
+        <div class="">
+
+        </div>
     </div>
 </template>
 
