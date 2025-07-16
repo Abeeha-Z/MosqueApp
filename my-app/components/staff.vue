37327251653a3bf049eb1f65f7a0d50e3f9bdc78
joe-238 <171643026+joe-238@users.noreply.github.com>
2025-07-16 09:52:40 -0400

    jj

---
+++ my-app/components/staff.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+props = defineProps({
+    Staffs: {
+        type: Array,
+        required: true, 
+        default: () => [],
+    } 
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ my-app/layout/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <navbar/>
+    </div>
+</template>
+
+<script setup>
+import navbar from '~/components/appLayout/navbar.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
